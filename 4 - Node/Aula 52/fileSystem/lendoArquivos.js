8ffd0bd0bbf47477ae466de5d359b26a3f4956e2
Vinícius Gabriel Leitempergher <viniciusgabriel.com@gmail.com>
2021-07-30 15:52:32 -0300

    Upando Arquivos

---
+++ 4 - Node/Aula 52/fileSystem/lendoArquivos.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const fsPromises = require("fs/promises");
+const path = require("path");
+
+// Lendo as informações de um arquivo (Callback)
+fs.readFile(path.resolve(__dirname, "teste.txt"), (err, data) => {
+    if (err) {
+        return console.log(err.message);
+    }
+
+    // data é um Buffer: https://nodejs.org/docs/latest-v14.x/api/buffer.html#buffer_class_buffer
+    // Portanto precisamos converter para utf-8 para conseguirmos visualizar o conteúdo do arquivo.
+    console.log(data.toString("utf-8"));
+});
+
+// Lendo as informações de um arquivo (Promise)
+(async () => {
+    try {
+        const data = await fsPromises.readFile(path.resolve(__dirname, "teste.txt"));
+
+        console.log(data.toString("utf-8"))
+    } catch (err) {
+        console.log(err.message);
+    }
+})();
+
+/*
+Exercício
+
+1) Crie um script que leia o arquivo exercioNomes.txt e mostra todos os nomes que começam com a letra A
+presentes no arquivo.
+
+2) Crie uma função getUserByName(name) que retorne o usuário obtido através do arquivo "users.json". Caso o usuário
+não exista a função deve retornar undefined.
+
+Se o usuário existir mostrar as informações do usuário no seguinte formato:
+Usuário encontrado: 
+nome: [nome do usuário]
+idade: [idade do usuário]
+email: [email do usuário]
+
+Caso o usuário não existir mostrar a mensagem: "Usuário não foi encontrado."
+
+Obs.: Nome e sobrenome.
+*/
