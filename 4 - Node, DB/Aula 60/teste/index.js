c088ec35a5f166a59c54ceb0dea7d39e8fa2b42c
Vinícius Gabriel Leitempergher <viniciusgabriel.com@gmail.com>
2021-08-11 17:23:06 -0300

    Upando Arquivos

---
+++ 4 - Node, DB/Aula 60/teste/index.js
@@ -0,0 +1,64 @@
+const { Pool } = require("pg");
+const format = require("pg-format")
+
+const pool = new Pool({
+    host: 'localhost',
+    user: 'postgres',
+    password: 'admin',
+    database: 'node',
+});
+
+(async () => {
+    // try {
+    //     await pool.query(`
+    //         CREATE TABLE IF NOT EXISTS funcionarios (
+    //             id SERIAL PRIMARY KEY,
+    //             nome TEXT NOT NULL,
+    //             email TEXT NOT NULL UNIQUE,
+    //             telefone TEXT NOT NULL UNIQUE
+    //         );
+    //         CREATE TABLE IF NOT EXISTS enderecos (
+    //             id SERIAL PRIMARY KEY,
+    //             rua TEXT NOT NULL,
+    //             numero INTEGER NOT NULL,
+    //             cidade TEXT NOT NULL,
+    //             estado TEXT NOT NULL,
+    //             id_funcionario INTEGER NOT NULL REFERENCES funcionarios
+    //         );
+    //     `);
+    //     console.log("Tabelas criadas.");
+    // } catch (e) {
+    //     console.log(e.message);
+    // } finally {
+    //     pool.end();
+    // }
+    // try {
+    //     const nome = "pedro", email = "email@email.com", telefone = "(47) 9 9999-9999"
+
+    //     await pool.query(`
+    //             INSERT INTO funcionarios (nome, email, telefone)
+    //                 VALUES ($1, $2, $3)
+    //         `, [nome, email, telefone]);
+    // } catch (e) {
+    //     console.log(e.message);
+    // } finally {
+    //     pool.end();
+    // }
+
+    try {
+        const funcionarios = [
+            ["joão", "joão@email.com", "(47) 9 8888-8888"],
+            ["maria", "maria@email.com", "(47) 9 7777-7777"]
+        ];
+
+        const query = format("INSERT INTO funcionarios (nome, email, telefone) VALUES %L RETURNING *", funcionarios);
+
+        const res = await pool.query(query);
+
+        console.log(res.rows);
+    } catch (error) {
+        console.log(error.message);
+    } finally {
+        pool.end();
+    }
+})();
