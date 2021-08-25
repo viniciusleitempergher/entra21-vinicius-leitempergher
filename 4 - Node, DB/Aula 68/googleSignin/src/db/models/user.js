e408aa5e4679d54d405ce34ee0b1c9aaae4ed0b0
Vinícius Gabriel Leitempergher <viniciusgabriel.com@gmail.com>
2021-08-25 14:02:13 -0300

    Upando Arquivos

---
+++ 4 - Node, DB/Aula 68/googleSignin/src/db/models/user.js
@@ -0,0 +1,39 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    profile_url: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
