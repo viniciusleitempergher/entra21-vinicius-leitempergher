b9e818b8a0846d7a6d11e12e8d167b91e739c67f
Vinícius Gabriel Leitempergher <viniciusgabriel.com@gmail.com>
2021-08-26 13:44:45 -0300

    Upando Arquivos

--- 4 - Node, DB/Aula 68/googleSignin/src/middlewares/authentication.js
+++ 4 - Node, DB/Aula 68/googleSignin/src/middlewares/authentication.js
@@ -3,16 +3,17 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (permissions) => {
     return (req, res, next) => {
+        
         const authToken = req.headers.authorization || "";
+        
 
         if (!authToken) {
             next(createHttpError(401, "Token is missing"));
         }
 
-        const [, token] = authToken.split(" ");
-
         try {
-            const payload = jwt.verify(token, process.env.TOKEN_SECRET);
+            const payload = jwt.verify(authToken, process.env.TOKEN_SECRET);
+            console.log("\n\n\n" + payload + "\n\n\n");
             
             if (!permissions.includes(payload.role)) {
                 next(createHttpError(403, "You don't have permission"));    
